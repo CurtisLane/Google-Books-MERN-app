d5488ff5d7a6d48d9976d264ffdd4f5ac0f82466
Curtis Lane <clanelmt@gmail.com>
2020-08-19 18:04:02 -0400

    add files and folders for front and backend

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Access api routes
+app.use(routes);
+
+// Connect to mongoose database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Static assets for heroku
 if (process.env.NODE_ENV === "production") {
