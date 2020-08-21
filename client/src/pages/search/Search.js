542df46c1fe9ee3374ab6cf2a5536722a6836c2f
Curtis Lane <clanelmt@gmail.com>
2020-08-21 14:29:10 -0400

    update .gitignore

--- client/src/pages/search/Search.js
+++ client/src/pages/search/Search.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import './style.css'
 
 function Search() {
     return (
-        <div className="grid-x grid-margin-x">
-            <div className="callout primary cell small-10 small-offset-1">
-                <h5>(React) Google Books Search</h5>
-                <p>Search for and Save Books of Interest</p>
+        <>
+            <div className="grid-x grid-margin-x searchTopCallout">
+                <div className="callout primary cell small-10 small-offset-1">
+                    <h5>(React) Google Books Search</h5>
+                    <p>Search for and Save Books of Interest</p>
+                </div>
             </div>
-        </div>
-        
+            <div className="grid-x grid-margin-x searchFormCallout">
+                <div className="callout primary cell small-10 small-offset-1">
+                    <p>Book Search</p>
+                    <label>
+                        <small>Book</small>
+                        <input type='text' />
+                        <a class="button primary" href="/">Search</a>
+                    </label>                    
+                </div>
+            </div>
+            
+        </>
     )
 }
 
