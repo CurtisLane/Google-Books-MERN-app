93e20141ea2260bb23635a1e9326dbebf2ea1a57
Curtis Lane <clanelmt@gmail.com>
2020-08-21 14:04:08 -0400

    move react-router-dom into client folder, add content to search page

--- client/src/pages/search/Search.js
+++ client/src/pages/search/Search.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 function Search() {
-    return <h1>Search</h1>
+    return (
+        <div className="grid-x grid-margin-x">
+            <div className="callout primary cell small-10 small-offset-1">
+                <h5>(React) Google Books Search</h5>
+                <p>Search for and Save Books of Interest</p>
+            </div>
+        </div>
+        
+    )
 }
 
 export default Search
