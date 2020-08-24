41f37705ef60069e100a0d6d5b542bf2369c7c5e
Curtis Lane <clanelmt@gmail.com>
2020-08-23 20:10:03 -0400

    move images abomve titles

--- client/src/components/searchPage/Result.js
+++ client/src/components/searchPage/Result.js
@@ -4,10 +4,10 @@ function Result({title, authors, description, image, link}) {
     return (
         <div className="grid-x grid-margin-x searchFormCallout">
             <div className="callout primary cell small-10 small-offset-1">
+                {image ? <img src={image} alt={title} /> : <p>No Image Available</p>}
                 <p><strong>Title:</strong> {title}</p>
                 {authors.length ? <p><strong>Authors:</strong> {authors.join(', ')}</p> : <p>No Authors Available</p>}
                 {description ? <p><strong>Description:</strong> {description}</p> : <p>No Description Available</p>}
-                {image ? <img src={image} alt={title} /> : <p>No Image Available</p>}
                 {/* link and save buttons go here */}
             </div>
         </div>
